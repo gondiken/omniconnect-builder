34bb3efb4ae9e14e403a4aae386186c0d7cee3c6
gondiken <inarusjuan@gmail.com>
2025-06-15 10:02:39 +0200

    omni setup

--- src/components/ui/input.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => (
-  <input
-    {...props}
-    className={`p-2 border rounded-md focus:outline-none focus:ring ${className || ""}`.trim()}
-  />
-);
--- src/components/ui/textarea.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Textarea: React.FC<React.TextareaHTMLAttributes<HTMLTextAreaElement>> = ({ className, ...props }) => (
-  <textarea
-    {...props}
-    className={`p-2 border rounded-md focus:outline-none focus:ring ${className || ""}`.trim()}
-  />
-);
